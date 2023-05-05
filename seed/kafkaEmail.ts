707df3dc2939b4dd2a176b5807aa54f89e37e0f2
jrock-3 <justin.liao3@gmail.com>
2023-05-05 11:50:57 -0700

    Add pnpm run kafka:msg seed script

--- seed/kafkaEmail.ts
+++ seed/kafkaEmail.ts
@@ -7,16 +7,25 @@ console.log(
   `Attempting to produce messages in ${env.ENV} kafka topic with mock data.`
 );
 try {
-  const results = messages.map(
-    async (message) =>
-      await fetch(`https://${env.KAFKA_URL}/produce/Email-Notifs/${message}`, {
-        method: "GET",
-        headers: {
-          Authorization: `Basic ${env.KAFKA_USERNAME}:${env.KAFKA_PASSWORD}`,
-        },
-      })
+  const results = await Promise.all(
+    messages.map(
+      async (message) =>
+        await fetch(
+          `https://${env.KAFKA_URL}/produce/${env.KAFKA_TOPIC}/${message}`,
+          {
+            method: "GET",
+            headers: {
+              Authorization:
+                "Basic " +
+                Buffer.from(
+                  `${env.KAFKA_USERNAME}:${env.KAFKA_PASSWORD}`
+                ).toString("base64"),
+            },
+          }
+        )
+    )
   );
-  console.log(results);
+
   console.log("Mock kafka data has succesfully produced!");
 } catch (e) {
   console.error("There was an issue in producing mock data:", e);
